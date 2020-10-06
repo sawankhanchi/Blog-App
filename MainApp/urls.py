9d36e90e2f29c86a6c465f3017b1aeeb6d92bacd
Sujay Shekhar <sujayshekhar162@gmail.com>
2020-10-06 20:26:26 +0530

    added search app

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+    path('search/',include('search.urls',namespace='Search')),
 ]
 
 if settings.DEBUG:
